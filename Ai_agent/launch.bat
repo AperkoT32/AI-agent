f380df7c06484c368968f1218e936d4fa9defb4d
AperkoT32 <74184217+AperkoT32@users.noreply.github.com>
2025-07-20 11:25:03 +0300

    Update launch.bat

--- Ai_agent/launch.bat
+++ Ai_agent/launch.bat
@@ -13,24 +13,12 @@ IF NOT EXIST ".venv\Scripts\python.exe" (
     echo Checking and updating packages...
     ".venv\Scripts\pip.exe" install -r requirements.txt --upgrade
 )
-echo Checking spaCy language model...
-".venv\Scripts\python.exe" -c "import spacy; exit(0) if 'ru_core_news_lg' in spacy.util.get_installed_models() else exit(1)" 2>nul
-IF %ERRORLEVEL% NEQ 0 (
-    echo SpaCy model ru_core_news_lg not found. Downloading...
-    ".venv\Scripts\python.exe" -m spacy download ru_core_news_lg
-    IF %ERRORLEVEL% NEQ 0 (
-        echo Failed to download SpaCy model!
-        pause
-        exit /b 1
-    )
-    echo SpaCy model successfully downloaded.
-) ELSE (
-    echo SpaCy model ru_core_news_lg is already installed.
-)
 
-echo Installing PyTorch...
-".venv\Scripts\pip.exe" install torch
+echo Устанавливаю модель spaCy ru_core_news_lg (если уже есть — будет пропущено)...
+".venv\Scripts\python.exe" -m spacy download ru_core_news_lg
 
 echo Agent started! owo
 ".venv\Scripts\python.exe" main.py
-pause
+echo.
+echo Нажмите любую клавишу для выхода...
+pause >nul
